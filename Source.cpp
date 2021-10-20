828c75c021ce7f37ab4eb8fdded2004b2113a996
mostafam7md <mostafamohamed19070@gmail.com>
2021-10-20 15:28:22 +0200

    first

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x, y, z;
+	cout << "please entre two num " << endl;
+	cin >> x >> y;
+	z = x + y;
+	cout << z << endl;
+	system("pause");
+	return 0;
+}
